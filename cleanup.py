a78c4c0fb67ca3b3a709b3e3bc14192daf2e85b8
Woomy <woomy@woomy.be>
2021-05-21 18:46:29 +0200

    Main: Check if arguments are regular files/dirs

--- cleanup.py
+++ cleanup.py
@@ -2,9 +2,14 @@
 
 from sys import argv, exit
 from lib.colorp import printc
+from os.path import isdir, isfile
 
 if __name__ == "__main__":
     if len(argv) < 2:
         printc("No arguments!", 31)
         exit(1)
+    
+    usepath = argv[1]
+    if not isdir(usepath) and not isfile(usepath):
+        printc(f"Unable to determine file type of {usepath}!", 31)
 
