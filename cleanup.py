8563e07eb305a0859d31d142b9144f2f1e12ab9f
Woomymy <github@woomy.ovh>
2021-05-21 18:43:07 +0200

    Main: Check arguments

--- cleanup.py
+++ cleanup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 
+from sys import argv, exit
 from lib.colorp import printc
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        printc("No arguments!", 31)
+        exit(1)
+
