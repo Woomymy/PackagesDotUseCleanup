3074606d124f994849e08ef7412db738f3776931
Woomymy <github@woomy.ovh>
2021-05-21 19:04:15 +0200

    Packages: Cleanup package list (remove comments)

--- cleanup.py
+++ cleanup.py
@@ -2,7 +2,7 @@
 
 from sys import argv, exit
 from lib.colorp import printc
-from lib.getpackageslist import get_packages_list
+from lib.getpackageslist import get_packages_list, clean_packages_list
 from os.path import isdir, isfile
 
 if __name__ == "__main__":
@@ -14,3 +14,5 @@ if __name__ == "__main__":
     if not isdir(usepath) and not isfile(usepath):
         printc(f"Unable to determine file type of {usepath}!", 31)
     packages = get_packages_list(usepath)
+    packages = clean_packages_list(packages)
+    
