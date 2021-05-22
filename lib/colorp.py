7b1800866a1c25c7f48f40948b81071ce61a51f3
Woomymy <github@woomy.ovh>
2021-05-22 12:17:23 +0200

    Linting: Fix pylint errors in lib/*

--- lib/colorp.py
+++ lib/colorp.py
@@ -1,3 +1,18 @@
+"""
+Color-printing related module
+"""
+
 from sys import stdout
-def printc(string, color, out=stdout):
+# Use 97(white) as default color
+def printc(string, color=97, out=stdout):
+    """
+    Print a text in color using ANSI escape codes
+    Example:
+    ```python
+    from sys import stderr
+    red_ansi_code = 31
+    printc("Hello", red_ansi_code) # Will print "Hello" on standart output
+    printc("Hello, world", red_ansi_code, stderr) # Will print "Hello, world" on the stderr
+    ```
+    """
     print(f"\x1b[{color}m{string} \x1b[m", file=out)
