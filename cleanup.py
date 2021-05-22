d30c598365d4c8b2653b9523c5a3f050b6054358
Woomy <woomy@woomy.be>
2021-05-22 12:08:21 +0200

    Linting: Fix pylint errors in cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 
-from sys import argv, exit, stderr
+"""
+Simple tool to cleanup /etc/portage/package.use(/*)
+"""
+
+# We import "exit" as "cexit" because pylint
+from sys import argv, exit as cexit
+from os.path import isdir, isfile
 from lib.colorp import printc
 from lib.getpackageslist import get_packages_list
 from lib.sort import sort_packages
-from os.path import isdir, isfile
 
 if __name__ == "__main__":
     if len(argv) < 2:
         printc("No arguments!", 31)
-        exit(1)
-    
+        cexit(1)
+
     usepath = argv[1]
     if not isdir(usepath) and not isfile(usepath):
         printc(f"Unable to determine file type of {usepath}!", 31)
+        cexit(1)
     packages = get_packages_list(usepath)
     filecontent = sort_packages(packages)
     print(filecontent)
